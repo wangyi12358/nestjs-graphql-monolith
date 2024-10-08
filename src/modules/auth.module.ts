32c57fd2970559c52ececa8e1c6e00cd282538e0
Mika Wang <1721436982@qq.com>
2024-10-08 14:18:57 +0800

    feat: add graphql

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,12 +1,12 @@
-import { JWT } from '@app/shared/constants/jwt';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { JWT_SECRET } from '~/constants/jwt';
 
 @Module({
   imports: [
     JwtModule.register({
       global: true,
-      secret: JWT.SECRET,
+      secret: JWT_SECRET,
       signOptions: { expiresIn: '2 days' },
     }),
   ],
--- src/resolvers/auth.resolver.ts
+++ src/resolvers/auth.resolver.ts
@@ -1,4 +1,9 @@
-import { Resolver } from "@nestjs/graphql";
+import { Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  @Query(() => String)
+  async hello() {
+    return 'Hello World!';
+  }
+}
